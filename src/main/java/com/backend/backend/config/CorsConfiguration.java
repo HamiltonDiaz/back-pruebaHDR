55c49a0a357bb7cfa6394f68b019a59ce6c59075
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2024-07-12 02:58:23 -0500

    configuracion final

--- src/main/java/com/backend/backend/config/CorsConfiguration.java
+++ src/main/java/com/backend/backend/config/CorsConfiguration.java
@@ -1,13 +1,19 @@
 package com.backend.backend.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebSecurity
+@CrossOrigin(origins= "*")
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
